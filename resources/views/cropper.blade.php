01d6aa11b5e8c6404defe0850da6ae4db5461152
Anson <anson@anttoweb.com>
2019-03-17 10:48:08 +0800

    优化help-block显示

--- resources/views/cropper.blade.php
+++ resources/views/cropper.blade.php
@@ -30,11 +30,12 @@
     <div class="{{$viewClass['field']}}">
 
         @include('admin::form.error')
+        <div class="input-group">
         <div data-id="{{$id}}" class="btn btn-info pull-left cropper-btn">浏览</div>
         <input id="{{$id}}-file" {!! $attributes !!} data-id="{{$id}}" class="cropper-file" type="file" accept="image/*"/>
         <img id="{{$id}}-img" {!! $attributes !!} data-id="{{$id}}" class="cropper-img" {!! empty($value) ? '' : 'src="'.old($column, $value).'"'  !!}>
         <input id="{{$id}}-input" type="hidden" name="{{$name}}" value="{{ old($column, $value) }}" {!! $attributes !!} />
-
+        </div>
         @include('admin::form.help-block')
 
     </div>
