58154124db927a9c9e6e5e4560feeb35b2706d34
Павел Мамаев <38722519+MamaikAn@users.noreply.github.com>
2020-12-26 22:51:20 +0300

    Fixes #33
    With required attribute in .cropper-file, form unable to submit. Swapping attribute from .cropper-file to .cropper-input probably fixes this issue.

--- resources/views/cropper.blade.php
+++ resources/views/cropper.blade.php
@@ -5,9 +5,9 @@
         @include('admin::form.error')
         <div class="btn btn-info pull-left cropper-btn">{{ trans('admin_cropper.choose') }}</div>
         @include('admin::form.help-block')
-        <input class="cropper-file" type="file" accept="image/*" {!! $attributes !!}/>
+        <input class="cropper-file" type="file" accept="image/*"/>
         <!-- <img class="cropper-img" {!! empty($value) ? '' : 'src="'.old($column, $value).'"'  !!}> -->
         <img class="cropper-img" {!! empty($value) ? '' : 'src="'.$preview.'"'  !!}>
-        <input class="cropper-input" name="{{$name}}" value="{{ old($column, $value) }}"/>
+        <input class="cropper-input" name="{{$name}}" value="{{ old($column, $value) }}" {!! $attributes !!}/>
     </div>
 </div>
